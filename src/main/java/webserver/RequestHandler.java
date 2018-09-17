e0e832a23ee99d1645f8cdff0f36832ef6ddd6c7
wonho <lwonho@hotmail.com>
2018-09-17 12:43:36 +0900

    302코드를 활용한 redirect

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -48,18 +48,16 @@ public class RequestHandler extends Thread {
 				}else if(type.equals("POST")){
 					HttpRequestUtils.getPostUser(br, line);
 				}
-				url = "/index.html";
+				
+				DataOutputStream dos = new DataOutputStream(out);
+				response302Header(dos);
+			}else {
+				DataOutputStream dos = new DataOutputStream(out);
+				byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+				response200Header(dos, body.length);
+				responseBody(dos, body);
 			}
 			
-			/*while(!line.equals("")) {
-				log.debug("header : {}", line);
-				line = br.readLine();
-			}*/
-			
-			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-			response200Header(dos, body.length);
-			responseBody(dos, body);
 		} catch (IOException e) {
 			log.error(e.getMessage());
 		}
@@ -70,6 +68,16 @@ public class RequestHandler extends Thread {
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
 			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
 			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+	
+	private void response302Header(DataOutputStream dos) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 OK \r\n");
+			dos.writeBytes("Location: /index.html\r\n");
 			dos.writeBytes("\r\n");
 		} catch (IOException e) {
 			log.error(e.getMessage());
